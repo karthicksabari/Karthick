5e5b6c4a03ad04fdc42cbdb270343d516f257010
karthick <karthicksabri123@gmail.com>
2023-12-30 10:03:45 +0530

    my first commit

---
+++ src/main/java/com/idtag/salaar/SalaarApplication.java
@@ -0,0 +1,13 @@
+package com.idtag.salaar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idtag/salaar/SalaarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idtag.salaar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
