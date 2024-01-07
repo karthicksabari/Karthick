e02b0f5ffff45a6115cf2e2b631d9813ae134689
karthick <karthicksabri123@gmail.com>
2024-01-07 16:56:52 +0530

    this is custome exception.

--- src/main/java/com/idtag/salaar/controller/ProjectController.java
+++ src/main/java/com/idtag/salaar/controller/ProjectController.java
@@ -1,14 +1,9 @@
 package com.idtag.salaar.controller;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
--- src/main/java/com/idtag/salaar/service/ProjectService.java
+++ src/main/java/com/idtag/salaar/service/ProjectService.java
@@ -4,16 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.idtag.salaar.entity.Project;
+import com.idtag.salaar.projectException.ProjectException;
 import com.idtag.salaar.repository.ProjectRepository;
 
 @Service
 public class ProjectService {
-	
+
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
 	public Project saveOrUpdateProject(Project project) {
-		return projectRepository.save(project);
+		try {
+			project.setProjectIdentifier(project.getProjectIdentifier());
+			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectException("the project ID '" + project.getProjectIdentifier() + "' already exists.");
+		}
 	};
 
 }
